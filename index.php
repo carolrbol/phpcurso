d202cef96632b8bce85c9cfdd1215d1ce949241c
carolrbol <caroline0liveira.ribeir07@gmail.com>
2025-04-09 01:01:38 -0300

    php teste

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$valor1 = 6;
+
+
+if(($valor % 2) == 0){
+    echo "<h2 style = 'color: blue;'> o numero ".$valor1." é par!!</h2>";
+
+
+}
+
+
+
+    else {
+     echo "<h2 style = 'color: red;'> o numero ".$valor." é par!!</h2>";
+
+    }
+
+
+
+
+?>
+
